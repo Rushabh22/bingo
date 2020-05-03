1c8594c9a8e533e7690b63295682d8ce80c52803
Rushabh Shah <shahrushabh87@gmail.com>
2020-05-03 10:53:57 +0800

    A bingo server side application to generate random numbers between 0 to 90, Allows verification of the numbers Allows to view the GeneratedNumbers

--- src/main/java/com/bingo/BingoApi.java
+++ src/main/java/com/bingo/BingoApi.java
@@ -1,40 +1,49 @@
 package com.bingo;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api("Housie Fun App")
 public class BingoApi {
     RandomNumberController randomNumberController = RandomNumberController.getInstance();
 
     @GetMapping("/bingo")
+    @ApiOperation("Generate lucky number")
     public LuckyNumber luckyNumber() {
         return new LuckyNumber(randomNumberController.generateLuckyNumber().toString());
     }
 
-    @GetMapping("/generated")
+    @GetMapping("/sorted")
+    @ApiOperation("Check the generated numbers")
     public GeneratedNumbers generatedNumber() {
         return randomNumberController.numbersTillNow();
     }
 
     @GetMapping("/generatedInSequence")
+    @ApiOperation("Check the generated numbers")
     public GeneratedNumbers generatedNumberInSequence() {
         return randomNumberController.numberInOrder();
     }
 
     @GetMapping("/lastTen")
+    @ApiOperation("Check the last ten generated numbers")
     public GeneratedNumbers generatedLastTen() {
         return randomNumberController.lastTen();
     }
 
     @GetMapping("/verify")
+    @ApiOperation("Verify the numbers, enter comma separated numbers to verify")
     public Verification verify(@RequestParam(value = "numbers") String numbers) {
         return randomNumberController.verify(numbers);
     }
 
     @DeleteMapping("/reset")
+    @ApiOperation("Reset the numbers and generate it again")
     public GeneratedNumbers reset() {
         return randomNumberController.reset();
     }
